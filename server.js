daaf9f0d60fea41a2822e21479c9bcec44651a1f
Chung Nguyen <chung.nguyen@sap.com>
2013-08-23 14:00:55 +0700

    added 2 tests regarding new game

--- server.js
+++ server.js
@@ -1,17 +1,50 @@
 var vnc = {};
+vnc.Piece = {
+  color: ['black', 'white'],
+  WHITE: 1,
+  BLACK: 0,
+  START: {
+    Tg: 'a5', S: ['a4','a6'], T: ['a3','a7'], M: ['a2','a8'], X: ['a1','a9'],
+    B: ['d1','d3','d5','d7','d9'], P: ['c2','c8']
+  }
+};
 
 vnc.Server = function() {
-  this.board = {
-    white: {},
-    black: {},
-    turn: 'white',
-    wplayer: 'chung',
-    bplayer: 'son',
-    history: []
+  this.board = new vnc.Board();
+  this.users = [];
+  this.join = function(person) {
+    this.users.push(person);
+    if (this.users.length >= 2) {
+      this.board.newGame(this.users);
+    }
   };
-  
-  this.move = function(m) {
-    console.log(m);
-  }
-
 };
+
+vnc.Board = function() {
+  this.turn = 0;
+  this.color = function() { return vnc.Piece.color[this.turn]; };
+  this.history = [];
+  this.newGame = function(users) {
+    this.white = this.black = vnc.Piece.START;
+    this.turn = vnc.Piece.WHITE - this.turn;
+    this.history = [];
+    if (users) {
+      this.wplayer = users[0];
+      this.bplayer = users[1];
+    }
+  };
+  this.move = function(m) {
+    console.log(this.color() + ': ' + m);
+    this.history.push(m);
+    this.turn = vnc.Piece.WHITE - this.turn;
+  };
+};
+
+
+var b = new vnc.Server().board;
+b.newGame();
+b.move('P2-5'); b.move('P2-5');
+b.move('M2-3'); b.move('M8-7');
+
+
+//console.log(s.board.black);
--- spec/ServerSpec.js
+++ spec/ServerSpec.js
@@ -1,11 +1,42 @@
 describe("Chess server", function() {
-  var server;
+  var s;
   beforeEach(function() {
-    server = new vnc.Server();
+    s = new vnc.Server();
   });
 
-  it("should have a board initialized", function() {
-    expect(server.board).toNotEqual(undefined);
+  it("should have no user initially", function() {
+    expect(s.users.length).toEqual(0);
   });
 
+  it("should start a new game if there are 2 users", function() {
+    s.join('chung');
+    s.join('son');
+    expect(s.users.length).toEqual(2);
+    expect(s.board.white).toEqual(vnc.Piece.START);
+    expect(s.board.black).toEqual(vnc.Piece.START);
+    expect(s.board.turn).toEqual(vnc.Piece.WHITE);
+  });
+});
+
+
+describe("Chess board", function() {
+  var b;
+  beforeEach(function() {
+    var server = new vnc.Server();
+    b = server.board;
+  });
+
+  it("should have been initialized correctly when start new game", function() {
+    b.newGame();
+    expect(b.white).toEqual(vnc.Piece.START);
+    expect(b.black).toEqual(vnc.Piece.START);
+    expect(b.turn).toEqual(vnc.Piece.WHITE);
+  });
+
+  it("should alternate side if start new game again", function() {
+    b.newGame();
+    expect(b.turn).toEqual(vnc.Piece.WHITE);
+    b.newGame();
+    expect(b.turn).toEqual(vnc.Piece.BLACK);
+  });
 });
